c769e2f5f8702260233b32190a06aec820b614f4
k-channon-PA <kevin.channon@paconsulting.com>
2022-04-25 14:52:29 +0100

    Add aliases for buffer container type and value type

--- tdd/buffer.h
+++ tdd/buffer.h
@@ -4,23 +4,25 @@ namespace kjc_ma {
 
 class Buffer {
  public:
-  using size_type = typename std::vector<uint8_t>::size_type;
+  using value_type = uint8_t;
+
+ private:
+  using _container_type = std::vector< value_type >;
+
+ public:
+  using size_type = typename std::vector< value_type >::size_type;
 
   Buffer() : _data(16, 0) {}
 
-  Buffer(std::initializer_list< uint8_t > x) : _data{std::move(x)}, _next_index{_data.size()} {};
+  Buffer(std::initializer_list< value_type > x) : _data{std::move(x)}, _next_index{_data.size()} {};
 
   [[nodiscard]] size_type size() const noexcept { return _next_index; }
   [[nodiscard]] size_type capacity() const noexcept { return _data.size(); }
-  void push(uint8_t val) { _data[_next_index++] = val; }
+  void push(value_type val) { _data[_next_index++] = val; }
 
  private:
-  std::vector< uint8_t> _data;
+  _container_type _data;
   size_type _next_index{0};
-
-
 };
-
-
 
 }  // namespace kjc_ma
